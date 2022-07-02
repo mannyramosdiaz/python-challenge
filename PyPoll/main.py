6575f4f2c8368f9fbf8c5c4f467da069ad83cfb8
mannyramosdiaz <65262422+mannyramosdiaz@users.noreply.github.com>
2022-07-02 16:19:15 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+#declaring lists to collect data
 Tally = []
 votecount = []
 winner = []
@@ -8,26 +8,26 @@ candidates = []
 cancount = []
 count = []
 vote_percent = []
-
+#using database to read csv
 election_csv = os.path.join("..","Resources", "election_data.csv")
 with open(election_csv,newline="") as csvfile:      
         csvreader = csv.reader(csvfile, delimiter=",")
         csv_header = next(csvreader)
-
+#collecting all the votes
         for row in csvreader:
             Tally.append(row[2])
-            
+#going through all votes, counter counts for each candidates and votecount is a list for each candidate
         for i in set(Tally):
             candidates.append(i)
             counter = Tally.count(i)
             votecount.append(counter)
             avg = round(counter/len(Tally)*100, 3)
             vote_percent.append(avg)
-
+#winner picks the largest of the votecount, winnerindex gives back the list element and winner calls on it.
 winnerindex = votecount.index(max(votecount))
 winner = candidates[winnerindex]
 print(winner)
-
+#print results and right a file with them
 with open("ElectionResults.txt","w") as file:
     l = ("Election Results\n"
     "-------------------------\n"
